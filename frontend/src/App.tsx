ca5e125c19c246a3f80fd9758db88434d4f22f29
Pablo <pablodiegodemuner@gmail.com>
2021-05-04 00:59:35 -0400

    Componentes basicos criados

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
 
+import DataTable from 'components/DataTable';
+import Footer from 'components/Footer';
+import NavBar from 'components/NavBar';
+import React from 'react';
 
 function App() {
   return (
-    <div >
+    <>
+    <NavBar />
+    <div className="container">
      <h1 className="text-primary">Olá Mundo!!</h1>
+     <DataTable />
     </div>
+    <Footer />
+    </>
   );
 }
 
