577bb877fde0665eff8530485a372fece238e7da
Pablo <pablodiegodemuner@gmail.com>
2021-05-09 18:19:16 -0400

    Integração do gráfico de barra Ok

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,12 @@
+//Classe TS que efetua a busca dos dados no Back-End
+
 export type SaleSum = {
-    sellerName: string,
-    sum: number
+    sellerName: string;
+    sum: number;
+}
+
+export type SaleSucces = {
+    sellerName: string;
+    visited: number;
+    deals: number;
 }
