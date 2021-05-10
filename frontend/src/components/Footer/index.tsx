6aaf3d6e2713e471ab17e23bbafd915671462313
Pablo <pablodiegodemuner@gmail.com>
2021-05-10 11:27:18 -0400

    Manutenção Footer

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -3,8 +3,8 @@ const Footer = () => {
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="https://github.com/PabloDemuner" target="_blank" rel="noreferrer">Pablo Demuner</a></p>
-                <p className="text-light"><small><strong>Spring React</strong><br />
-            Para mais informações sobre mim acesse: <a href="https://www.linkedin.com/in/pablo-diego-demuner-costa-31695b1b7" target="_blank" rel="noreferrer">LinkedIn</a></small></p>
+                <p className="text-light"><small>
+            Para mais informações sobre acesse: <a href="https://www.linkedin.com/in/pablo-diego-demuner-costa-31695b1b7" target="_blank" rel="noreferrer">LinkedIn</a></small></p>
             </div>
         </footer>
     );
